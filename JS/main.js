47245d893dec5fd5fe21830132a3190996e53474
Georgeave <67703102+Georgeave@users.noreply.github.com>
2021-08-05 00:10:28 +0400

    change button style

--- JS/main.js
+++ JS/main.js
@@ -5,15 +5,22 @@ const addBtn = document.querySelector('.addBtn')
 const input = document.querySelector('.input-area');
 const todoHeader = document.querySelector('.todo-header');
 
+let tasksArr = []; 
 let countItems = () => { 
     count = list.childElementCount;
     todoHeader.innerHTML = "You have " + count + " Tasks";
 }
 countItems();
 
-input.addEventListener('keyup', ()=>{
-    todoText = input.value;
-})
+let createAndPushObj = (objText) =>{    
+    let obj = {
+        text: "",
+        id: ""
+    }
+    obj.text = objText;
+    obj.id = (tasksArr.length) + Math.floor(Math.random() * 99) + objText.charAt(0); 
+    tasksArr.push(obj);  
+}
 
 let createItem = (e) => {
     e.preventDefault();
@@ -27,35 +34,46 @@ let createItem = (e) => {
 
     let button = document.createElement('button');
     button.classList = "remove-button";
+    button.textContent = 'X';
 
-    let i = document.createElement('i');
-    i.classList = "fas fa-times remove-image";
-   
-    button.appendChild(i);
     li.appendChild(h3);
     li.appendChild(button);
 
     if(input.value != undefined && input.value != ''){
         list.appendChild(li);
+        createAndPushObj(input.value); 
     } else{
         alert("Text must be entered!");
     }
-
     countItems();
     input.value = "";
     todoText = "";
 }
-
-addBtn.addEventListener('click', createItem);
+let removeItemFromArray = (item) => {
+    let index = tasksArr.indexOf(item) 
+    if (index > -1) {
+        tasksArr.splice(index, 1);
+      }
+}
 
 let removeItem = (e) => {
-    if(e.target.classList.contains("remove-button") || e.target.classList.contains("remove-image")){
-        let listItem = document.querySelector('.todo-list-item');     
-        list.removeChild(listItem);  
+    if(e.target.classList.contains("remove-button")){   
+        // remove element from an array !!!
+        let currentLi = e.target.parentElement;
+        if(currentLi.children[0].tagName == 'H3'){
+            let h3Text = currentLi.children[0].innerHTML;
+            // if tasksArr[obj]["text"] ==  h3text then remove obj
+            tasksArr.map((item) => item.text == h3Text && removeItemFromArray(item));
+        }
+        // remove element from list 
+        let listItem = e.target.parentElement;  
+        list.removeChild(listItem);       
     }
     countItems();
 }
 
+input.addEventListener('input', () => todoText = input.value)
+addBtn.addEventListener('click', createItem);
 list.addEventListener('click', removeItem);
 
 
