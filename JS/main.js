9ec96af9b34479ed7ac53ac7796d0f6f5749ca8a
Georgeave <67703102+Georgeave@users.noreply.github.com>
2021-08-05 13:37:11 +0400

    feat: create a local storage

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,22 @@
-let todoText, count;
 const list = document.querySelector('.todo-list');
 const removeBtn = document.querySelector('.remove-button');
 const addBtn = document.querySelector('.addBtn')
 const input = document.querySelector('.input-area');
 const todoHeader = document.querySelector('.todo-header');
+let todoText, count;
 
-let tasksArr = []; 
-let countItems = () => { 
+const countItems = () => { 
     count = list.childElementCount;
     todoHeader.innerHTML = "You have " + count + " Tasks";
 }
 countItems();
 
-let createAndPushObj = (objText) =>{    
-    let obj = {
-        text: "",
-        id: ""
-    }
-    obj.text = objText;
-    obj.id = (tasksArr.length) + Math.floor(Math.random() * 99) + objText.charAt(0); 
-    tasksArr.push(obj);  
-}
+document.addEventListener('DOMContentLoaded', renderLocalTasks);
+input.addEventListener('input', () => todoText = input.value)
+addBtn.addEventListener('click', createItem);
+list.addEventListener('click', removeItem);
 
-let createItem = (e) => {
+function createItem(e){
     e.preventDefault();
 
     let li = document.createElement('li');
@@ -41,40 +35,74 @@ let createItem = (e) => {
 
     if(input.value != undefined && input.value != ''){
         list.appendChild(li);
-        createAndPushObj(input.value); 
+        saveLocalTasks(todoText);
+        input.value = "";
+        todoText = "";
     } else{
         alert("Text must be entered!");
     }
     countItems();
-    input.value = "";
-    todoText = "";
-}
-let removeItemFromArray = (item) => {
-    let index = tasksArr.indexOf(item) 
-    if (index > -1) {
-        tasksArr.splice(index, 1);
-      }
 }
 
-let removeItem = (e) => {
+function removeItem(e){
     if(e.target.classList.contains("remove-button")){   
-        // remove element from an array !!!
-        let currentLi = e.target.parentElement;
-        if(currentLi.children[0].tagName == 'H3'){
-            let h3Text = currentLi.children[0].innerHTML;
-            // if tasksArr[obj]["text"] ==  h3text then remove obj
-            tasksArr.map((item) => item.text == h3Text && removeItemFromArray(item));
+        let listItem = e.target.parentElement;
+        if(listItem.children[0].tagName == 'H3'){     
+            let todo = listItem.children[0].innerHTML;
+            removeLocalTasks(todo);
         }
-        // remove element from list 
-        let listItem = e.target.parentElement;  
         list.removeChild(listItem);       
     }
     countItems();
 }
 
-input.addEventListener('input', () => todoText = input.value)
-addBtn.addEventListener('click', createItem);
-list.addEventListener('click', removeItem);
+function saveLocalTasks(todo){
+    let tasks = [];
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }  else {
+        tasks =  JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(todo);
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+}
+
+function renderLocalTasks(){
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }  else {
+        tasks =  JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach((todo) => {
+        let li = document.createElement('li');
+        li.classList = "todo-list-item";
+
+        let h3 = document.createElement('h3');
+        h3.classList = "item-name";      
+        h3.innerHTML = todo;
+
+        let button = document.createElement('button');
+        button.classList = "remove-button";
+        button.textContent = 'X';
+
+        li.appendChild(h3);
+        li.appendChild(button);
+        list.appendChild(li);
+    });
+    countItems();
+}
+
+function removeLocalTasks (todo){
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }  else {
+        tasks =  JSON.parse(localStorage.getItem('tasks'));
+    }
+    indexOfTodo = tasks.indexOf(todo);
+    tasks.splice(indexOfTodo,1);
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+}
+
 
 
 
